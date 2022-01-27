23f49d7be57cba09c986f584ec73264590405277
Muhta3 <89270569+Muhta3@users.noreply.github.com>
2022-01-27 14:14:46 -0500

    comments

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -28,8 +28,8 @@ class BankAccountTest {
         assertFalse( BankAccount.isEmailValid(""));
 
         //These are my tests you need to make pass in MY repository -Laci
-        assertFalse(BankAccount.isEmailValid("a@bdotcom")); //No . in email --- This is an invalid partition, this is testing a border case for the domain --.com
-        assertFalse(BankAccount.isEmailValid("a.b@com"));  // the . came before the @ --- This is an invalid partition,  this is testing a border case and an invalid equivalence test
+        assertFalse(BankAccount.isEmailValid("a@bdotcom")); //No . in email --- This is an invalid partition, this is testing a boundary case for the domain --.com
+        assertFalse(BankAccount.isEmailValid("a.b@com"));  // the . came before the @ --- This is an invalid partition,  this is testing a boundary case and an invalid equivalence test
 
         //We did this backwards initially...this test you wrote should be in your test file
         //in your repository
