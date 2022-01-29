0f4f1e277d80dd250ceab2a781a091b9eb7b6c06
Laci Young <lyoung4@ithaca.edu>
2022-01-28 21:10:44 -0500

    getBalance tests

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -28,6 +28,8 @@ public class BankAccount {
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * If amount is negative, throw exception "not enough money"
+     * If amount is larger than balance, throw exception "not enough money"
      */
     public void withdraw (double amount) throws InsufficientFundsException{
         if (amount <= balance){
